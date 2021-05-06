ff24d3be83f340cfb5fa5acd367d803c9866c19e
gabijagleiz <79048184+gabijagleiz@users.noreply.github.com>
2021-05-06 15:45:04 +0300

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -20,33 +20,43 @@ using namespace std::chrono;
 //const int k = 100;
 const char DFV[] = "failas1000.txt";
 
-struct Studentas {
-	string var;    //vardas
-	string pav;    //pavarde
-	int nkiek;
-	vector <int> nd;  //namu darbu rezultatas
-	int egz;    //egzamino rezultatas
-	double vid;
-	double med;
-	double gr;  //galutinis rezultatas
+class Studentas {
+	private:
+		string vardas, pavarde;    //vardas, pavarde
+		int nk;          //nd kiekis
+		vector <int> nd;  	//namu darbu rezultatas
+		int egz;			//egzamino rezultatas
+		double vid, med, gr;
+	public:    
+		Studentas() {vardas = "", pavarde = "", ndkiekis = 0, egzaminas = 0, Vidurkis = 0, Mediana = 0, Galutinis = 0};
+		Studentas(string vardas, string pavarde, int ndkiekis, vector <int> nd, int egzaminas, double Vidurkis, double Mediana
+		double Galutinis);
+		
+		void setStudent(string, string, int);
+		string getname() const {return var;}
+		string getLastname() const {return pav;}
+		int getExam() const {return egz;}
+		int getFinal() const {return gr;}
+		vector <int> gethomework() const {return nd;}
 	
 };
 
 void sort (vector <Studentas> S, int k);
-bool CompareLastNames(Studentas& a, Studentas& b);
+/*bool CompareLastNames(Studentas& a, Studentas& b);
 
 template <class T>
-void rikiavimas(T& );
+void rikiavimas(T& );*/
 
+bool yra_protingas(Studentas& S);
 double vidurkis(vector <Studentas> S, int k);
 double mediana (vector <Studentas> S, int k);
 
 
 template <class T>
-void skirstymas(T&, T&, T&, int k);
+void skirstymas(T&, T&, T&);
 
 template <class T>
-void skirstymas(T&, T&, int k);
+void skirstymas(T&, T&);
 
 
 template <class T>
@@ -70,6 +80,4 @@ void genskaitymas(T&, T&, T&);
 template <class T>
 void duomenys(T&, T&, T&);
 
-
 void galutinis(char);
-
