e935755a62b1d60e5d422f6dadf8963362578cb3
gabijagleiz <79048184+gabijagleiz@users.noreply.github.com>
2021-05-20 15:44:33 +0300

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -13,55 +13,10 @@
 #include <fstream>
 #include <sstream>
 #include <iomanip>
-#include "person.h"
+#include "studentas.h"
 
 using namespace std;
 using namespace std::chrono;
-
-
-
-class Studentas : public Asmuo {
-	private:
-		int nkiek;          //nd kiekis
-		vector <int> nd;  	//namu darbu rezultatas
-		int egz;			//egzamino rezultatas
-		double vid, med, gr;
-	public:    
-		Studentas() {Asmuo::var = "", Asmuo::pav = "", nkiek = 0, egz = 0, vid = 0, med = 0, gr = 0;}
-
-		int getnumber() const{return nkiek;}
-		int getExam() const {return egz;}
-		int getFinal() const {return gr;}
-		vector <int> gethomework() const {return nd;}
-
-		void setnumber(int kiekis) {nkiek = kiekis;}
-		void setexam(int egzaminas) {egz = egzaminas;}
-		void setfinal(double galutinisrez) {gr = galutinisrez;}
-		void sethomework(vector <int> namudarbas) {nd = namudarbas;}
-	
-
-		~Studentas(){} //Destructor
-
-		// Copy Constructor
-		Studentas(const Studentas& that):
-			nkiek(that.nkiek),
-			nd(that.nd), egz(that.egz), gr(that.gr) {}
-		
-
-		// copy assignment Operator
-		Studentas& operator= (const Studentas& that){
-			
-			var = that.var;
-			pav = that.pav;
-			nkiek = that.nkiek;
-			nd = that.nd;
-			egz = that.egz;
-			gr = that.gr;
-		return *this;
-	}
-};
-
-
 
 
 void sort (vector <Studentas> S, int k);
